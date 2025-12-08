c80be2eb5e741938151d59b50de39730b4e87ad5
Jared Haertel <jared.haertel@gmail.com>
2025-12-08 09:27:42 -0800

    Removed build files and bashrc

--- build/planning/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/cc/ee106a/fa25/class/ee106a-adc/EECS-C106A-Final-Project/install/robotiq_hande_description/share/robotiq_hande_description/package.sh"
--- build/robotiq_hande_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106a/fa25/class/ee106a-adc/EECS-C106A-Final-Project/src/robotiq_hande_description
-# Build directory: /home/cc/ee106a/fa25/class/ee106a-adc/EECS-C106A-Final-Project/build/robotiq_hande_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
