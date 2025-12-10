043343dd88aa65a902f18137146f45e719537dc6
jared_haertel <jared.haertel@berkeley.edu>
2025-12-10 02:00:12 -0800

    depth extracted

---
+++ src/ball_sense/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ball_sense'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ee106a-aal',
+    maintainer_email='jared.haertel@berkeley.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ball_sense = ball_sense.ball_sense:main'
+        ],
+    },
+)
