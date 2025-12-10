18b26d03820c1dbcfa7390bd81d9b4203f61b8ae
Jared Haertel <jared.haertel@gmail.com>
2025-12-10 02:24:23 -0800

    Fixed sim

--- src/planning/planning/offline_sim.py
+++ src/planning/planning/offline_sim.py
@@ -12,7 +12,9 @@ from .trajectory_generation.generate_samples import solve_by_sampling
 import tyro
 import os
 from robot_descriptions.loaders.yourdfpy import load_robot_description
-
+from .load_urdf import UR7eJointVar
+import jax_dataclasses as jdc
+import jax.numpy as jnp
 
 def main():
     # Initialize robot
@@ -22,34 +24,46 @@ def main():
     # For UR5 it's important to initialize the robot in a safe configuration;
     default_cfg = np.array([4.712, -1.850, -1.425, -1.405, 1.593, -3.141]) 
     robot = pk.Robot.from_urdf(urdf, default_joint_cfg=default_cfg)
+    UR7eJointVar.default_factory = staticmethod(lambda: jnp.array(default_cfg))
+    robot = jdc.replace(robot, joint_var_cls=UR7eJointVar)
     target_link_name = "ee_link"
-
     # Initialize world
     world = World(robot, urdf, target_link_name)
 
     # Generate example trajectory
     start_cfg = default_cfg
-    target_pos = np.array([-0.3, 2.0, .7])
+    target_pos = np.array([-0.3, 2.0, .2])
     time_horizon = 1.19
     timesteps = 40
     dt = time_horizon / timesteps
 
-    
+    status = "regenerate"
+    traj, t_release, t_target = None, None, None
+    solutions = None
     while True:
-        traj, t_release, t_target = solve_by_sampling(
-            robot,
-            robot_coll,
-            world.gen_world_coll(),
-            target_link_name,
-            start_cfg,
-            target_pos,
-            timesteps,
-            dt,
-            robot_max_reach=0.85 * 0.8, # max 
-            max_vel=7, 
-            cache_dir=os.path.join('/Users/jhaertel/code/catchv22/src/planning', 'saved_problems'),
-            num_samples=50,
-        )
+        # Check if we need to solve again
+        print(status)
+        match status:
+            case "regenerate" | "next" if not solutions:
+                print("STARTING AGAIN!")
+                solutions = solve_by_sampling(
+                    robot,
+                    robot_coll,
+                    world.gen_world_coll(),
+                    target_link_name,
+                    start_cfg,
+                    target_pos,
+                    timesteps,
+                    dt,
+                    robot_max_reach=0.85 * 0.8, # max 
+                    max_vel=7, 
+                    num_samples=50,
+                    num_samples_iterated=10,
+                )
+                traj, t_release, t_target = solutions.pop(0)
+            case "next" if solutions:
+                traj, t_release, t_target = solutions.pop(0)
+
         status = world.visualize_all(
                 start_cfg, 
                 target_pos, 
@@ -59,12 +73,7 @@ def main():
                 timesteps, 
                 dt
             )
+        print(status)
 
-        # Check if we need to solve again
-        if "regenerate" == status: 
-            print("STARTING AGAIN!")
-            continue
-
-    
 if __name__ == "__main__":
     tyro.cli(main)
